3eda69b1b6e07c95f1b87c1bc860a4edd1342c55
Matt <matt-jbl@hotmail.co.uk>
2025-06-06 15:40:24 +0100

    Nicer component-based markup

--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -1,15 +1,14 @@
 import { fetchData } from "@/utils/api";
 import { InitialValue } from "@/types/InitialValue";
+import Result from "./Result";
 
 export default async function ResultsList() {
 
     const results = await fetchData("http:localhost:3001/api/get");
-    // console.log("Results:", results);
 
     const resultsList = results.map((result: InitialValue) =>
-        <li>{result.url}: {result.initialValue}</li>        
+        <Result url={result.url} initialValue={result.initialValue}></Result>
     )
-    
 
     return (
         <div>
