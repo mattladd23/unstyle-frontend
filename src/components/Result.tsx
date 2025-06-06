5bba346d7271572465af71b2b3607ceafc470876
Matt <matt-jbl@hotmail.co.uk>
2025-06-06 15:56:38 +0100

    Had to add the semi-colon

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -4,7 +4,7 @@ import getPropertyName from "@/utils/getPropertyName";
 export default function Result({ url, initialValue }: InitialValue) {
     return (
         <li>
-            <code>{getPropertyName(url)}: {initialValue}</code>
+            <code>{getPropertyName(url)}: {initialValue};</code>
             <br/>
             <small>Read more at <a href={url}>MDN Web Docs</a></small>
         </li>
