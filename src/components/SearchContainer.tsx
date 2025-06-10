2c1d7f654d48723ac67d2115bb409782c4ee421d
Matt <matt-jbl@hotmail.co.uk>
2025-06-10 16:37:08 +0100

    Getting and passing user input

--- src/components/SearchContainer.tsx
+++ src/components/SearchContainer.tsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useRef } from "react";
+
 export default function SearchContainer() {
 
     const placeholderText : string = `E.g. "padding-bottom"`;
+    const userInput = useRef<HTMLInputElement>(null);
+
+    const getUserInput = ():string => {
+        if (userInput.current) {
+            return userInput.current.value.trim();
+        }
+        return "";
+    }
+
+    const printUserInput = ():void => {
+        const input = getUserInput();
+        if (input) {
+            console.log(`User input: ${input}`);
+        } else {
+            console.log("No input provided.");
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-between my-4 p-4">
@@ -9,6 +30,8 @@ export default function SearchContainer() {
                 type="text"
                 placeholder={placeholderText}
                 className="flex-grow p-2 border bg-white border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-600 text-gray-900 hover:shadow hover:shadow-gray-300"
+                ref={userInput}
+                onKeyUp={printUserInput}
             />
         </div>
     );
