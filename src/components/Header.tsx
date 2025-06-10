0f91f7f8d84848fea020b6a8c969f075251d38d1
Matt <matt-jbl@hotmail.co.uk>
2025-06-10 12:02:18 +0100

    Fontawesome icons working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRotateLeft } from '@fortawesome/free-solid-svg-icons';
+
 export default function Header() {
 
     return (
         <header className="grid grid-cols-12 col-span-12 bg-teal-700">
-            <h1 className="col-span-12 md:col-start-0 md:col-end-2 text-2xl font-bold text-white mx-auto my-0 p-4">Unstyle.io</h1>
+            <h1 className="flex col-span-12 md:col-start-0 md:col-end-2 text-2xl font-bold text-white mx-auto my-0 p-4">
+                Unstyle.io
+                <FontAwesomeIcon className="w-5 h-5 pl-1" icon={faArrowRotateLeft} />
+            </h1>
+            <FontAwesomeIcon className="col-span-12 md:col-start-11 md:col-end-13 h-5 w-5 my-auto text-white" icon={faSquareGithub} />
         </header>
     )
 }
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,7 @@
 import { InitialValue } from "@/types/InitialValue";
 import getPropertyName from "@/utils/getPropertyName";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function Result({ url, initialValue }: InitialValue) {
     return (
@@ -8,7 +10,7 @@ export default function Result({ url, initialValue }: InitialValue) {
                 {getPropertyName(url)}
             </h3>
             <code className="inline-block bg-gray-300 font-black rounded-md mb-2 px-1 cursor-pointer hover:outline hover:outline-gray-600">{getPropertyName(url)}: {initialValue};</code>
-            <small className="block text-gray-900 p-1">Read more at <a className="text-orange-600 underline underline-offset-1 hover:no-underline" href={url} target="_blank">MDN Web Docs</a></small>
+            <small className="block text-gray-900 p-1">Read more at <a className="text-orange-600 underline underline-offset-1 hover:no-underline" href={url} target="_blank">MDN Web Docs<FontAwesomeIcon className="inline-block pb-1 pl-1 w-3 h-3" icon={faArrowUpRightFromSquare} /></a></small>
         </li>
     );
 }
