6cd22503272f94257eeb051592b7b29fa3ed1153
Matt <matt-jbl@hotmail.co.uk>
2025-06-18 16:59:42 +0100

    Added blank targets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
             <small className="col-span-12 md:col-start-1 md:col-end-4">All property and initial value data sourced from <MozillaLink url={url} /> - © Mozilla.</small>
             <small className="col-span-12 md:col-start-12 md: col-end-13">
                 <FontAwesomeIcon className="col-span-12 md:col-start-11 md:col-end-13 h-5 w-5 my-auto text-gray-900" icon={faSquareGithub} />
-                Github repos: <a href={githubAPIUrl} className="text-orange-600 underline underline-offset-1 hover:no-underline mr-1">API<FontAwesomeIcon className="inline-block pb-1 pl-1 mr-1 w-3 h-3" icon={faArrowUpRightFromSquare} /></a> <a href={githubFrontendUrl} className="text-orange-600 underline underline-offset-1 hover:no-underline">Frontend<FontAwesomeIcon className="inline-block pb-1 pl-1 mr-1 w-3 h-3" icon={faArrowUpRightFromSquare} /></a>
+                Github repos: <a href={githubAPIUrl} target="_blank" className="text-orange-600 underline underline-offset-1 hover:no-underline mr-1">API<FontAwesomeIcon className="inline-block pb-1 pl-1 mr-1 w-3 h-3" icon={faArrowUpRightFromSquare} /></a> <a href={githubFrontendUrl} target="_blank" className="text-orange-600 underline underline-offset-1 hover:no-underline">Frontend<FontAwesomeIcon className="inline-block pb-1 pl-1 mr-1 w-3 h-3" icon={faArrowUpRightFromSquare} /></a>
             </small>
         </footer>
     )
