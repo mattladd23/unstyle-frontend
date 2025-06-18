a50529142cd6bd40af077f3d976445765c47c2cf
Matt <matt-jbl@hotmail.co.uk>
2025-06-18 12:27:06 +0100

    Results rendering logic working well

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 
     return (
         <header className="grid grid-cols-12 col-span-12 bg-teal-700">
-            <h1 className="flex col-span-12 md:col-start-0 md:col-end-2 text-2xl font-bold text-white mx-auto my-0 p-4">
+            <h1 className="flex col-span-12 md:col-start-0 md:col-end-2 text-2xl font-bold text-white mx-auto my-0 p-4 grow-0">
                 <span className="text-orange-600">un</span>style.io
                 <FontAwesomeIcon className="w-5 h-5 pl-1" icon={faArrowRotateLeft} />
             </h1>
