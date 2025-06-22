ca3de14bd5624eddebc1b2e1af45c19bd5469467
Matt <matt-jbl@hotmail.co.uk>
2025-06-22 19:19:12 +0100

    UI working at child component level

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -2,28 +2,39 @@ import { InitialValue } from "@/types/InitialValue";
 import getPropertyName from "@/utils/getPropertyName";
 import MozillaLink from "./MozillaLink";
 import { useCopyToClipboard } from "usehooks-ts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Result({ url, initialValue }: InitialValue) {
-    
+
     const propertyName: string = getPropertyName(url);
     const codeSnippet: string = `${propertyName}: ${initialValue};`;
 
     const [copiedCode, copy] = useCopyToClipboard();
-    const handleCopy = (code: string) => () => {
-        copy(code);
+    const handleCopy = () => {
+        copy(codeSnippet);
     }
+    const isCopied: boolean = copiedCode === codeSnippet;
 
     return (
         <li className="rounded-xl p-4 mb-4 bg-white transition-transform hover:shadow hover:shadow-grey-500 hover:scale-102">
             <h3 className="text-xl font-bold text-teal-700 mb-2">
                 {propertyName}
             </h3>
-            <code
-                className="inline-block bg-gray-300 font-black rounded-lg mb-2 px-2 cursor-pointer transition-transform hover:outline hover:outline-gray-600"
-                onClick={handleCopy(codeSnippet)}    
+            <div
+                onClick={handleCopy}
+                title={isCopied ? "Copied!" : "Copy to clipboard"}
             >
-                {codeSnippet}
-            </code>
+                <code
+                    className="inline-block bg-gray-300 font-black rounded-lg mb-2 px-2 cursor-pointer transition-transform hover:outline"
+                >
+                    {codeSnippet}
+                </code>
+                <FontAwesomeIcon
+                    icon={isCopied ? faCheck : faCopy}
+                    className={`inline-block pb-1 pl-1 mr-1 w-3 h-3 cursor-pointer transition-transform ${isCopied ? "text-teal-700" : "text-gray-400 hover:text-gray-800"}`}
+                />
+            </div>
             <small className="block text-gray-900 p-1">Read more at <MozillaLink url={url}/></small>
         </li>
     );
