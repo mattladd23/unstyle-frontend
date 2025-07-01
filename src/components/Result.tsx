a3cec2cbf3b9d41127d7ef37215383dd6592f997
Matt <matt-jbl@hotmail.co.uk>
2025-07-01 15:05:28 +0100

    Dark mode copy icon white

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -32,7 +32,7 @@ export default function Result({ url, initialValue, onCopy, copiedSnippet }: Res
                 </code>
                 <FontAwesomeIcon
                     icon={isCopied ? faCheck : faCopy}
-                    className={`inline-block pb-1 pl-1 mr-1 w-3 h-3 cursor-pointer transition-transform ${isCopied ? "text-teal-700" : "text-gray-400 hover:text-gray-800 dark:hover:text-orange-600"}`}
+                    className={`inline-block pb-1 pl-1 mr-1 w-3 h-3 cursor-pointer transition-transform ${isCopied ? "text-teal-700" : "text-gray-400 hover:text-gray-800 dark:hover:text-white"}`}
                 />
             </button>
             <small className="block text-gray-900 dark:text-white p-1">Read more at <MozillaLink url={url}/></small>
