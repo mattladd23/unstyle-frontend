e8201561477ab68267d72398061183e87dce5360
Matt <matt-jbl@hotmail.co.uk>
2025-07-09 12:08:24 +0100

    Footer tests

--- src/components/MozillaLink.tsx
+++ src/components/MozillaLink.tsx
@@ -3,7 +3,11 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function MozillaLink({ url }: { url: string }) {
     return (
-        <a className="text-orange-600 underline underline-offset-1 hover:no-underline" href={url} target="_blank"
+        <a
+            className="text-orange-600 underline underline-offset-1 hover:no-underline"
+            href={url}
+            target="_blank"
+            data-testid="mozilla-link"
         >
             MDN Web Docs
             <FontAwesomeIcon
