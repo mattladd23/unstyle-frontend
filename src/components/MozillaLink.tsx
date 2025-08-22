5ab36cea82fc3dd9430d56a1faa77f3987cae144
Matt <matt-jbl@hotmail.co.uk>
2025-08-22 17:01:38 +0100

    Darker orange colour for a11y and removed magnifying glass

--- src/components/MozillaLink.tsx
+++ src/components/MozillaLink.tsx
@@ -4,7 +4,7 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 export default function MozillaLink({ url }: { url: string }) {
     return (
         <a
-            className="text-orange-600 underline underline-offset-1 hover:no-underline"
+            className="text-orange-700 underline underline-offset-1 hover:no-underline"
             href={url}
             target="_blank"
             data-testid="mozilla-link"
